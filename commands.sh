4903c55e995a206e4886e7eb7ca10c637afc4a29
berstearns <you@example.com>
2025-02-27 15:25:08 +0000

    update

---
+++ commands.sh
@@ -0,0 +1,16 @@
+case $1 in
+ cvt) # convert
+ :
+ ;;
+ # for f in ./*;do ffmpeg -i  -ar 16000 -ac 1 -c:a pcm_s16le .wav; done
+ seg) # segment
+ :
+ ;;
+ # ffmpeg -i TESTE_01_chatgpt.m4a.wav -f segment -segment_time 60 -c copy out%03d.wav
+ trcb) # transcribe
+ :
+ ;;
+ #~/venv/bin/whisper --output_dir --language pt ./extracted contents/out000.wav
+ *)
+ ;;
+esac
